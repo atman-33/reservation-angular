1ac83ab6ebcf249e5e6b95589bbe4956ec15e8ce
atman-33 <gpbjk0304@gmail.com>
2023-04-16 16:07:47 +0900

    ProductListComponentを追加

--- _documents/cmd-memo.md
+++ _documents/cmd-memo.md
@@ -11,3 +11,8 @@ npm install bootstrap@4 --save
               "node_modules/bootstrap/dist/css/bootstrap.min.css",
               "src/styles.scss"
             ],
+※sytlesは複数存在するので、全てに適用すること
+
+# Angularコンポーネント生成
+ng generate component component-name
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+/**
+ * SPA用の画面遷移先（path）設定
+ */
+const routes: Routes = [
+  { path: "", component: ProductListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
