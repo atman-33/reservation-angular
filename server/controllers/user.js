fc7967c302ed3968d64556a3819e4da5c962cd1c
atman-33 <gpbjk0304@gmail.com>
2023-04-28 15:46:55 +0900

    hotfix

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -21,7 +21,7 @@ exports.authMiddleware = function (req, res, next) {
     }
 
     // tokenが「Bearer ~~token~~」という形のデータを保持しているため、スペースでsplitしてtoken部分のみ取得 
-    jwt.verify(token.split(' ')[1], config.SERCRET, function (err, decodedToken) {
+    jwt.verify(token.split(' ')[1], config.SECRET, function (err, decodedToken) {
         if (err) {
             return res.status(401).json({ error: [{ title: 'Not Autorized', detail: 'Invalid token!' }] });
         }
